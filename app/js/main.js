21778d4fcd8ca829ea83c9f40103b9f4005ec6df
Bilchenko26 <Bilchenko026@gmail.com>
2021-05-11 11:59:27 +0300

    fixed contact form

--- app/js/main.js
+++ app/js/main.js
@@ -81,21 +81,3 @@ function slowScroll(id) {
   return false;
 }
 
-$(document).ready(function() {
-
-	$("form").submit(function() { 
-		let th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-});
